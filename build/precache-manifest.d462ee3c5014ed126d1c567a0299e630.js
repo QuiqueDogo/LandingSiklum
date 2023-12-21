5e7c58fab07f0d05dc3186a01a4047f03e05e6bc
QuiqueDogo <ochoafernando014@gmail.com>
2023-12-21 13:48:35 -0600

    cambios

---
+++ build/precache-manifest.d462ee3c5014ed126d1c567a0299e630.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "298f7cfa97a4a72f21fa",
+    "url": "/static/css/main.371f891f.chunk.css"
+  },
+  {
+    "revision": "298f7cfa97a4a72f21fa",
+    "url": "/static/js/main.da1bbb94.chunk.js"
+  },
+  {
+    "revision": "d2ec3b1fbcfb274eb30f",
+    "url": "/static/js/runtime~main.8a5ef2bf.js"
+  },
+  {
+    "revision": "8136f9d63576ce0c62d3",
+    "url": "/static/js/2.846bd098.chunk.js"
+  },
+  {
+    "revision": "0484309c6f7a3306e2c2",
+    "url": "/static/js/3.f531135f.chunk.js"
+  },
+  {
+    "revision": "61fc43d6b4261773cf34bcb71bfead40",
+    "url": "/static/media/checkwhite.61fc43d6.svg"
+  },
+  {
+    "revision": "3ba95f4eb56cb1864a6e841694f9f275",
+    "url": "/static/media/checkblue.3ba95f4e.svg"
+  },
+  {
+    "revision": "3d64e39f2b0de45e40efecf3c31b56ab",
+    "url": "/static/media/infowhite.3d64e39f.svg"
+  },
+  {
+    "revision": "58f200484a03f9e2c1a39ea02f182032",
+    "url": "/static/media/Logos.58f20048.svg"
+  },
+  {
+    "revision": "34db8af6ce55145b5cce668d3ab6f26e",
+    "url": "/static/media/mockup.34db8af6.png"
+  },
+  {
+    "revision": "5ff0adc50aceeb6a3bd28c1fd6055301",
+    "url": "/static/media/logos.5ff0adc5.png"
+  },
+  {
+    "revision": "9b24c2bfd8738a5a8b029b1c2fa34626",
+    "url": "/static/media/expediente_1.9b24c2bf.svg"
+  },
+  {
+    "revision": "82a8273b75d552e822936d56fff24317",
+    "url": "/static/media/expediente_2.82a8273b.svg"
+  },
+  {
+    "revision": "d3f4a0ddb2394ee9785f68d240de5d97",
+    "url": "/static/media/expediente_3.d3f4a0dd.svg"
+  },
+  {
+    "revision": "d92020ae096d494bba829ddec772733f",
+    "url": "/static/media/expediente_4.d92020ae.svg"
+  },
+  {
+    "revision": "725d9a60b0ca23f68118788c4ef608af",
+    "url": "/static/media/blue.725d9a60.svg"
+  },
+  {
+    "revision": "47e564432f90bf09ef1c577302722216",
+    "url": "/static/media/expediente.47e56443.svg"
+  },
+  {
+    "revision": "8d67e3e24f20f70aed4d4e58d7fe0d9b",
+    "url": "/static/media/consola.8d67e3e2.svg"
+  },
+  {
+    "revision": "f049eca3f48711601c08daa40797e7ba",
+    "url": "/static/media/pago.f049eca3.svg"
+  },
+  {
+    "revision": "6d639034b7bf0e8d87bcead58f8e08c2",
+    "url": "/static/media/clientes.6d639034.svg"
+  },
+  {
+    "revision": "de42a6448fd1bede9d62efd104009c02",
+    "url": "/static/media/buzon.de42a644.svg"
+  },
+  {
+    "revision": "b674476caa5992baa56e623c3f113c2c",
+    "url": "/static/media/mensaje.b674476c.svg"
+  },
+  {
+    "revision": "0f85b7fdbdef8eacf5b8934700283b56",
+    "url": "/static/media/demoplay.0f85b7fd.svg"
+  },
+  {
+    "revision": "bd560ddbaf441a26e1ddbe489d24401d",
+    "url": "/index.html"
+  }
+];
